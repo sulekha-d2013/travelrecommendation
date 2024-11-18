7f135a3abf580ac42232182d8370c4cd67adf39e
sulekha-d2013 <126109238+sulekha-d2013@users.noreply.github.com>
2024-11-19 00:42:51 +0530

    Add files via upload

---
+++ travel_recommendation.js
@@ -0,0 +1,27 @@
+    const btnSearch = document.getElementById('btnSearch');
+    function searchCondition() {
+        const input = document.getElementById('conditionInput').value.toLowerCase();
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = '';
+
+        fetch('./travel_recommendation_api.json')
+          .then(response => response.json())
+          .then(data => {
+            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+            if (condition) {
+
+              resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+              resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+              resultDiv.innerHTML += `<p><strong>Description:</strong> ${condition.description}</p>`;
+            } else {
+              resultDiv.innerHTML = 'Location not found.';
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+          });
+      }
+        btnSearch.addEventListener('click', searchCondition);
